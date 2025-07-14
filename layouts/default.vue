3dee0598bffa096d23c9f0fa53ef652136d5db53
jsh <jsh@curioud.com>
2025-07-14 16:25:46 +0900

    레이아웃 수정

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,23 @@
 <template>
-  <header>
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/list/1">List</NuxtLink>
-    <NuxtLink to="/editor/1">에디터</NuxtLink>
-    <NuxtLink to="/view/1">완성화면</NuxtLink>
-    <NuxtLink to="/payment">Payment</NuxtLink>
-    
-  </header>
-  <slot />
+  <div>
+    <header>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/list/1">List</NuxtLink>
+      <NuxtLink to="/editor/1">에디터</NuxtLink>
+      <NuxtLink to="/view/1">완성화면</NuxtLink>
+      <NuxtLink to="/payment">Payment</NuxtLink>
+    </header>
+    <slot />
+  </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+a{
+  margin-right: 10px;
+  text-decoration: none;
+  color: blue;
+}
 </style>
