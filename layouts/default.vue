c8ee52d2cce8aeba9f85418f266be2ca5bf90880
jsh <jsh@curioud.com>
2025-07-15 12:45:50 +0900

    app.vue에 있는 유저관리 dev로 옮김

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,8 @@
       <NuxtLink to="/list/1">List</NuxtLink>
       <NuxtLink to="/editor/1">에디터</NuxtLink>
       <NuxtLink to="/view/1">완성화면</NuxtLink>
-      <NuxtLink to="/payment">Payment</NuxtLink>
+      <NuxtLink to="/payment">결제</NuxtLink>
+      <NuxtLink to="/dev">개발</NuxtLink>
     </header>
     <slot />
   </div>
