0f4ade9aab94478e51624d079ccd84c14c93e17e
ksm <rkdtjd0909@gmail.com>
2025-07-19 00:01:09 +0900

    데이터 생성시 트랜잭션 처리

--- server/repositories/sign/signRepository.ts
+++ server/repositories/sign/signRepository.ts
@@ -2,17 +2,19 @@ import { Prisma } from "@prisma/client"
 import prisma from "~/lib/prisma"
 
 export const insertUser = async (data: Prisma.UserCreateInput) => {
-  return await prisma.user.create({
-    data: data,
-    select: {
-      no: true,
-      uuid: true,
-      email: true,
-      phoneNumber: true,
-      name: true,
-      nickname: true,
-      createdDt: true,
-      role: true,
-    }
+  return await prisma.$transaction(async (tx) => {
+    return await tx.user.create({
+      data: data,
+        select: {
+        no: true,
+        uuid: true,
+        email: true,
+        phoneNumber: true,
+        name: true,
+        nickname: true,
+        createdDt: true,
+        role: true,
+      }
+    })
   })
 }
