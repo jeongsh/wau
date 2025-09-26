019dbb597d6c33e0f3b134747e5304fe3c8ae827
정상훈 <jsh@curioud.com>
2025-09-26 14:21:22 +0900

    미들웨어 주석

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,20 +1,20 @@
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  const token = useCookie('access_token')?.value
-  const isDev = process.env.NODE_ENV === 'development'
+  // const token = useCookie('access_token')?.value
+  // const isDev = process.env.NODE_ENV === 'development'
 
-  if (isDev) return
+  // if (isDev) return
 
-  // 제외할 경로 설정
-  const excludedRoutes = [
-    '/',
-    '/login',
-  ]
-  if (excludedRoutes.includes(to.path)) return
+  // // 제외할 경로 설정
+  // const excludedRoutes = [
+  //   '/',
+  //   '/login',
+  // ]
+  // if (excludedRoutes.includes(to.path)) return
 
-  // 토큰 없으면 로그인 페이지로 리다이렉트
-  if (!token) {
-    return navigateTo('/dev/sign/login')
-    // return navigateTo('/login')
-  }
+  // // 토큰 없으면 로그인 페이지로 리다이렉트
+  // if (!token) {
+  //   return navigateTo('/dev/sign/login')
+  //   // return navigateTo('/login')
+  // }
 })
